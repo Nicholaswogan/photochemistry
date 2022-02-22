029ccb88ea27392bea85fcae2ba137c7e3eabe79
Nick Wogan <nicholaswogan@gmail.com>
2022-02-22 11:02:20 -0800

    data structs

--- src/example.cpp
+++ src/example.cpp
@@ -9,67 +9,126 @@ double MyClass::sum_arr()
 
 namespace Photochem {
   
-void SpeciesBase::net_production()
+
+void check_for_keys(YAML::Node n, std::string place, std::vector<std::string> &keys)
 {
-  
+  for (int j = 0; j < keys.size(); j++){
+    if (!n[keys[j]]){
+      throw PhotoException(place+" does not contain key \""+keys[j]+"\"");
+    }
+  }
 }
 
-void LongLivedGas_t::net_production()
+void check_for_key(YAML::Node n, std::string place, std::string key)
 {
-  std::cout << "LongLivedGas" << std::endl;
-}
-
-void Particle_t::net_production()
-{
-  std::cout << "Particle" << std::endl;
+  if (!n[key]){
+    throw PhotoException(place+" does not contain key \""+key+"\"");
+  }
 }
   
   
-}
-
-
-
-
-namespace Photochem {
 
 void read_stuff()
 {
+  PhotoData d;
+  
   YAML::Node file;
   std::string filename = "../photochemistry/data/reaction_mechanisms/zahnle_earth.yaml";
-  file = YAML::LoadFile(filename);
-  
-  
-  if (!file["atoms"]){throw PhotoException(filename+" is missing key atoms");}
-  YAML::Node atoms = file["atoms"];
-  std::vector<Atom> a(atoms.size());
-  for (int i = 0; i < atoms.size(); i++){
-    a[i].name = atoms[i]["name"].as<std::string>();
-    a[i].mass = atoms[i]["mass"].as<double>();
-    a[i].redox = atoms[i]["redox"].as<double>();
+  try{
+    file = YAML::LoadFile(filename);
+  }
+  catch (YAML::BadFile& e) {
+    throw PhotoException(filename+" does not exist.");
   }
   
-  if (!file["species"]){throw PhotoException(filename+" is missing key species");}
-  YAML::Node species = file["species"];
+  // Read all data
+  try{
   
   
-  std::vector<std::unique_ptr<SpeciesBase>> s;
-  s.reserve(2);
-  std::unique_ptr<Particle_t> p = std::make_unique<Particle_t>();
-  std::unique_ptr<LongLivedGas_t> g = std::make_unique<LongLivedGas_t>();
-  p->name = "hi";
-  p->type = Particle;
-  p->b = 100;
-  g->name = "hiiiii";
-  g->type = LongLivedGas;
-  g->a = 101;
-  s.push_back(std::move(p));
-  s.push_back(std::move(g));
+  { // atoms
   
-  std::cout << s[0]->type << std::endl;
-  std::cout << static_cast<Particle_t&>(*s[0]).name << std::endl;
+  check_for_key(file, filename+"/", "atoms");
+  YAML::Node a = file["atoms"];
   
-  s[0]->net_production();
-  s[1]->net_production();
+  std::vector<std::string> keys = {"name", "mass", "redox"};
+  d.natoms = a.size();
+  d.atoms.resize(d.natoms);
+  for (int i = 0; i < a.size(); i++){
+    check_for_keys(a[i], filename+"/atoms["+std::to_string(i)+"]", keys);
+    
+    d.atoms[i].name = a[i]["name"].as<std::string>();
+    d.atoms[i].mass = a[i]["mass"].as<real>();
+    d.atoms[i].redox = a[i]["redox"].as<real>();
+  } 
+  
+  } // end atoms
+  
+  { // species
+  
+  check_for_key(file, filename+"/", "species");
+  YAML::Node s = file["species"];
+  
+  std::vector<std::string> keys = {"name", "composition", "thermo"};
+  d.species.resize(s.size());
+  for (int i = 0; i < s.size(); i++){
+    check_for_keys(s[i], filename+"/species["+std::to_string(i)+"]", keys);
+    
+    // determine type of species
+    
+    std::unique_ptr<LongLivedGas_t> g = std::make_unique<LongLivedGas_t>();
+    
+    // common components of all species
+    std::string sp = s[i]["name"].as<std::string>();
+    g->name = sp;
+    g->composition.resize(d.natoms);
+    g->mass = 0.0;
+    g->redox = 0.0;
+    for (int j = 0; j < d.natoms; j++){
+      if (s[i]["composition"][d.atoms[j].name]){
+        g->composition[j] = s[i]["composition"][d.atoms[j].name].as<int>();
+        g->mass += g->composition[j]*d.atoms[j].mass;
+        g->redox += g->composition[j]*d.atoms[j].redox;
+      } else {
+        g->composition[j] = 0;
+      }
+    }
+    
+    // Components specific to each species type
+    
+    
+    d.species[i] = std::move(g);
+    
+  } 
+    
+  } // end species
+
+    
+    
+  std::cout << d.species.size() << std::endl;
+    
+    
+    
+    
+    
+  
+  
+  // s.reserve(2);
+  // std::unique_ptr<Particle_t> p = std::make_unique<Particle_t>();
+  // std::unique_ptr<LongLivedGas_t> g = std::make_unique<LongLivedGas_t>();
+  // p->name = "hi";
+  // p->type = Particle;
+  // p->b = 100;
+  // g->name = "hiiiii";
+  // g->type = LongLivedGas;
+  // g->a = 101;
+  // s.push_back(std::move(p));
+  // s.push_back(std::move(g));
+  // 
+  // std::cout << s[0]->type << std::endl;
+  // std::cout << static_cast<Particle_t&>(*s[0]).name << std::endl;
+  // 
+  // s[0]->net_production();
+  // s[1]->net_production();
   
   // SpeciesVector s;
   
@@ -78,6 +137,22 @@ void read_stuff()
   //   std::cout << key << std::endl;
   // }
   
+  
+  }
+  catch (YAML::BadConversion& e) {
+    throw PhotoException(e.what());
+  }
+  
+}
+
+void read_stuff_wrapper()
+{
+  try{
+    read_stuff();
+  }
+  catch (YAML::BadConversion& e) {
+    throw PhotoException(e.what());
+  }
 }
 
 
--- src/example.h
+++ src/example.h
@@ -8,10 +8,13 @@
 #include <xtensor/xmath.hpp>
 #include <xtensor/xio.hpp>
 
+#define real REAL_KIND
+
 class MyClass
 {
 public:
   xt::xarray<double> arr {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  xt::xarray<double> arr1;
   double sum_arr();  
 };
 
@@ -29,54 +32,237 @@ public:
   }
 }; 
 
-struct Atom
+/////////////////////////
+// Boundary conditions //
+/////////////////////////
+enum {DepositionVelocity, FixedMixingRatio, FixedFlux, VdepDistributedFlux};
+
+struct BoundaryCondition_t
 {
-  std::string name;
-  double mass;
-  double redox;
+  size_t type;
+  float vdep;
+  float fixedmr;
+  float flux;
+  float disth;
 };
 
+//////////////
+// Settings //
+//////////////
+
+struct Settings
+{
+  // atmosphere-grid
+  real bottom;
+  real top;
+  size_t number_of_layers;
+  
+  // photolysis-grid
+  real lower_wavelength;
+  real upper_wavelength;
+  size_t number_of_bins;
+  
+  // planet
+  bool use_background_gas;
+  std::string background_gas;
+  real surface_pressure;
+  real planet_mass;
+  real planet_radius;
+  real surface_albedo;
+  real diurnal_averaging_factor;
+  real solar_zenith_angle;
+  bool diff_lim_hydrogen_escape;
+  // gas rainout
+    bool gas_rainout;
+    real rainfall_rate;
+    real tropopause_altitude;
+    
+  // particle condensation rates
+  
+  
+  // boundary conditions and species types
+  std::vector<std::string> species_names;
+  std::vector<size_t> species_type;
+  std::vector<BoundaryCondition_t> lower_bc;
+  std::vector<BoundaryCondition_t> upper_bc;
+  
+};
+
+///////////
+// Atoms //
+///////////
+struct Atom_t
+{
+  std::string name;
+  real mass;
+  real redox;
+};
+
+/////////////
+// Species //
+/////////////
 enum {Particle, LongLivedGas, ShortLivedGas, BackgroundGas};
 
-struct SpeciesBase 
+// 
+struct SpeciesBase_t
 {
   size_t type;
   std::string name;
   std::vector<size_t> iprod;
   std::vector<size_t> iloss;
   
-  std::vector<size_t> composition;
-  double mass;
-  double redox;
-  size_t thermo;
-  size_t henry;
+  std::vector<int> composition;
+  real mass;
+  real redox;
   
   // Production/Loss mechanisms
   //   1. reactions
   //   2. rainout
   //   3. condensation/evaporations
   
-  virtual void net_production();
-
+  // virtual void net_production();
 };
 
-struct LongLivedGas_t: public SpeciesBase
+struct Particle_t: public SpeciesBase_t
 {
-  int a;
-  void net_production();
+  
 };
 
-struct Particle_t: public SpeciesBase
+struct LongLivedGas_t: public SpeciesBase_t
 {
-  int b;
-  void net_production();
+  
 };
 
+struct ShortLivedGas_t: public SpeciesBase_t
+{
+  
+};
 
+struct BackgroundGas_t: public SpeciesBase_t
+{
+  
+};
 
+///////////////
+// Reactions //
+///////////////
 
+struct Efficiencies_t
+{
+  size_t n_eff;
+  size_t def_eff;
+  std::vector<real> efficiencies;
+  std::vector<size_t> eff_sp_inds;
+};
+
+enum {PhotolysisReaction, ElementaryReaction, ThreeBodyReaction, FalloffReaction, ReverseReaction};
+
+struct ReactionBase_t
+{
+  size_t type;
+  std::string equation;
+  size_t nreact;
+  size_t nprod;
+  std::vector<size_t> react_sp_inds;
+  std::vector<size_t> prod_sp_inds;
+  size_t reverse_info; 
+  
+  // computes rate for all heights in atmosphere
+  // virtual void reaction_rate(xt::xarray<real> &temp, 
+  //                            xt::xarray<real> &gibbsE, 
+  //                            xt::xarray<real> &density,
+  //                            xt::xarray<real> &densities,
+  //                            xt::xarray<real> *forward_rates,
+  //                            xt::xarray<real> &rx_rates);
+};
+
+struct PhotolysisReaction_t: public ReactionBase_t
+{
+};
+
+struct ElementaryReaction_t: public ReactionBase_t
+{
+  real A;
+  real b;
+  real Ea;
+};
+
+struct ThreeBodyReaction_t: public ReactionBase_t
+{
+  real A;
+  real b;
+  real Ea;
+  Efficiencies_t eff; // efficiencies
+};
+
+struct FalloffRate_t: public ReactionBase_t
+{
+  real A0;
+  real b0;
+  real Ea0;
+  real Ainf;
+  real binf;
+  real Eainf;
+  
+  size_t falloff_type;
+  real A_T;
+  real T1;
+  real T2;
+  real T3;
+  
+  Efficiencies_t eff; // efficiencies
+};
+
+struct ReverseReaction_t: public ReactionBase_t
+{
+  // if a reversed reaction, 
+  // then it is the index of the forward
+};
+
+struct PhotoData
+{
+  // atoms
+  size_t natoms;
+  std::vector<Atom_t> atoms;
+  
+  // species
+  // Organization is as follows
+  // [       nsp       ]
+  // [   nq   + nsl + 1]
+  // [np + ng + nsl + 1]
+  // |_______|
+  //     |
+  // Only np + ng = nq evolve through time.
+  size_t np;   // number of particles that evolve throught time
+  size_t ng;   // number of gas-phase species that evolve through time
+  size_t ng_1; // index of first gas
+  size_t nq;   // number of speices that evolve through time
+  size_t nsl;  // number of short-lived gas moleules.
+  size_t nsp;  // total number of species
+  std::vector<std::string> species_names; // copy of species names
+  std::vector<std::unique_ptr<SpeciesBase_t>> species;
+  
+  // reactions
+  bool reverse; // True if there are reverse reactions
+  size_t nrF; // number of forward reactions
+  size_t nrR; // number of reverse reactions
+  size_t nrT; // number of total reactions
+  std::vector<std::string> reaction_equations; // copy of reaction names
+  std::vector<std::unique_ptr<ReactionBase_t>> reactions;
+  
+};
+
+struct PhotoVars
+{
+  // PhotoVars can change change between integrations
+  // but not during a single integration.
+  
+  // boundary conditions
+  std::vector<BoundaryCondition_t> lower_bc; // nq in length
+  std::vector<BoundaryCondition_t> upper_bc; // nq in length
+
+};
 
 void read_stuff();
 
-
-}
+} // Photochem namespace
